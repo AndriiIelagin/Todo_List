d53c7ea93a925fa0fd9518ded4d362ee8b74a4f1
Andrii Ielagin <andriiielagin@gmail.com>
2018-09-13 17:17:27 +0000

    Make changes for deploying on Heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+var url = process.env.DATABASEURL;
+mongoose.connect(url);
 
 mongoose.Promise = Promise;
 
